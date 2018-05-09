6ae70141e32b9907b92d168417b7326ff0fea65d
Pan-Charakternik <35796629+Pan-Charakternik@users.noreply.github.com>
2018-05-09 12:34:25 +0200

    Update 2

--- src/com/jjozwik/Main.java
+++ src/com/jjozwik/Main.java
@@ -2,56 +2,82 @@ package com.jjozwik;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        try{
 
-        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
-        printFile(path);
+            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+            Files.delete(fileToDelete);
 
-//        Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
-        Path filePath = FileSystems.getDefault().getPath(".","files","SubdirectoryFile.txt");
-        printFile(filePath);
+            // Rename/Move
 
-        filePath = Paths.get("C:\\Users\\Jakub_Jozwik\\IdeaProjects\\OutThere.txt");
-        printFile(filePath);
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file2.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Files.move(fileToMove, destination);
 
-        filePath = Paths.get(".");
-        System.out.println(filePath.toAbsolutePath()+"\n");
+            //Copy file
 
-        Path path2 = FileSystems.getDefault().getPath(".","files","..","files","SubdirectoryFile.txt");
-        printFile(path2);
+//            Path sourceFile =  FileSystems.getDefault().getPath("Examples","file1.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+//            Files.copy(sourceFile, copyFile,StandardCopyOption.REPLACE_EXISTING);
+//
+//
+//            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
+//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.copy(sourceFile,copyFile, StandardCopyOption.REPLACE_EXISTING);
 
-        System.out.println(path2.normalize().toAbsolutePath());
-        printFile(path2.normalize());
-
-
-        Path path3 = FileSystems.getDefault().getPath("nonexisting.txt ");
-        System.out.println(path3.toAbsolutePath());
-
-        Path path4 = Paths.get("D:\\","abcdef.dat");
-        System.out.println(path4.toAbsolutePath());
-
-        filePath = FileSystems.getDefault().getPath("files");
-        System.out.println("Exits = " + Files.exists(filePath));
-
-        System.out.println("Exisis = " + Files.exists(path4));
-    }
-
-    private static void printFile(Path path){
-        try(BufferedReader fileReader = Files.newBufferedReader(path)){
-            String line;
-            while ((line = fileReader.readLine()) != null){
-                System.out.println(line);
-            }
         } catch (IOException e){
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
-        System.out.println("-----------------");
     }
 }
+
+//        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+//        printFile(path);
+//
+////        Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
+//        Path filePath = FileSystems.getDefault().getPath(".","files","SubdirectoryFile.txt");
+//        printFile(filePath);
+//
+//        filePath = Paths.get("C:\\Users\\Jakub_Jozwik\\IdeaProjects\\OutThere.txt");
+//        printFile(filePath);
+//
+//        filePath = Paths.get(".");
+//        System.out.println(filePath.toAbsolutePath()+"\n");
+//
+//        Path path2 = FileSystems.getDefault().getPath(".","files","..","files","SubdirectoryFile.txt");
+//        printFile(path2);
+//
+//        System.out.println(path2.normalize().toAbsolutePath());
+//        printFile(path2.normalize());
+//
+//
+//        Path path3 = FileSystems.getDefault().getPath("nonexisting.txt ");
+//        System.out.println(path3.toAbsolutePath());
+//
+//        Path path4 = Paths.get("D:\\","abcdef.dat");
+//        System.out.println(path4.toAbsolutePath());
+//
+//        filePath = FileSystems.getDefault().getPath("files");
+//        System.out.println("Exits = " + Files.exists(filePath));
+//
+//        System.out.println("Exisis = " + Files.exists(path4));
+//
+//
+//    }
+//
+//    private static void printFile(Path path){
+//        try(BufferedReader fileReader = Files.newBufferedReader(path)){
+//            String line;
+//            while ((line = fileReader.readLine()) != null){
+//                System.out.println(line);
+//            }
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+//        System.out.println("-----------------");
+//    }
+//}
