d68a99f53d6a571a2d6de5cf9061008bb60962af
Pan-Charakternik <35796629+Pan-Charakternik@users.noreply.github.com>
2018-05-09 16:08:27 +0200

    Update 2

--- src/com/jjozwik/Main.java
+++ src/com/jjozwik/Main.java
@@ -3,22 +3,54 @@ package com.jjozwik;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
 
     public static void main(String[] args) {
         try{
 
-            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
-            Files.delete(fileToDelete);
+            Path filePath = FileSystems.getDefault().getPath("Examples", "Dir1/file1.txt");
+            long size = Files.size(filePath);
+            System.out.println("Size = " + size);
+            System.out.println("Last modified = " + Files.getLastModifiedTime(filePath));
 
-            // Rename/Move
+            BasicFileAttributes attrs = Files.readAttributes(filePath, BasicFileAttributes.class);
+            System.out.println("Size = " + attrs.size());
+            System.out.println("Last modified = " + attrs.lastModifiedTime());
+            System.out.println("Created = " + attrs.creationTime());
+            System.out.println("isDirectory = " + attrs.isDirectory());
+            System.out.println("isReqularFile = " + attrs.isRegularFile());
+
+
+
+            //CREATE
+            //directories:
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples","Dir2/Dir3/Dir4/Dir5/Dir6");
+//            Files.createDirectories(dirToCreate);
+
+            //folder:
+
+//            Path fileToCreate = FileSystems.getDefault().getPath("Examples", "file2.txt");
+//            Files.createFile(fileToCreate);
+
+            //directory:
+
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.createDirectory(dirToCreate);
+
+            //DELETE
+//
+//            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+//            Files.delete(fileToDelete);
+
+            //RENAME/MOVE
 
 //            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file2.txt");
 //            Path destination = FileSystems.getDefault().getPath("Examples", "file1.txt");
 //            Files.move(fileToMove, destination);
 
-            //Copy file
+            //COPY/REPLACE
 
 //            Path sourceFile =  FileSystems.getDefault().getPath("Examples","file1.txt");
 //            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
