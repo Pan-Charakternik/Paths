a1c175a282420d00ed4e5d587b7e3fbb0e8d7dfc
Pan-Charakternik <35796629+Pan-Charakternik@users.noreply.github.com>
2018-05-10 11:26:53 +0200

    Update 2

--- src/com/jjozwik/Main2.java
+++ src/com/jjozwik/Main2.java
@@ -1,10 +1,9 @@
 package com.jjozwik;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.util.Iterator;
 
 public class Main2 {
 
@@ -12,11 +11,34 @@ public class Main2 {
 
     public static void main(String[] args) {
 
-        Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory)){
+//        DirectoryStream.Filter<Path> filter =
+//                new DirectoryStream.Filter<Path>(){
+//                    @Override
+//                    public boolean accept(Path path) throws IOException {
+//                        return (Files.isRegularFile(path));
+//                    }
+//                };
+
+        DirectoryStream.Filter<Path> filter = p -> Files.isRegularFile(p);
+
+        Path directory = FileSystems.getDefault().getPath("FileTree"+File.separator+"Dir2");
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)){
             for(Path file: contents){
                 System.out.println(file.getFileName());
             }
+        } catch (IOException | DirectoryIteratorException e){
+            System.out.println(e.getMessage());
+        }
+
+        String separator = File.separator;
+        System.out.println(separator);
+        separator = FileSystems.getDefault().getSeparator();
+        System.out.println(separator);
+
+        try{
+            Path tempFile = Files.createTempFile("myapp",".appext");
+            System.out.println("Temporary file path = " + tempFile.toAbsolutePath());
+
         } catch (IOException e){
             System.out.println(e.getMessage());
         }
